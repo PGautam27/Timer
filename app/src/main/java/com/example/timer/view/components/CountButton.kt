ccf8125101b36b673e62434c7a64904dd4a29cdb
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-03-03 15:38:45 +0530

    Jetpack timer

---
+++ app/src/main/java/com/example/timer/view/components/CountButton.kt
@@ -0,0 +1,13 @@
+package com.example.timer.view.components
+
+import androidx.compose.runtime.Composable
+import java.lang.reflect.Modifier
+
+@Composable
+fun CountButton(
+    modifier: Modifier,
+    optionSelected: () -> Unit,
+    isPlaying: Boolean
+) {
+
+}
