9b5628977d8ff2724c0def2bc5d1f6e49a49844c
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-03-08 15:24:25 +0530

    Jetpack timer

--- app/src/main/java/com/example/timer/model/model.kt
+++ app/src/main/java/com/example/timer/model/model.kt
@@ -1,6 +1,11 @@
 package com.example.timer.model
 
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import com.example.timer.Data.timeList
+import com.example.timer.service.INTENT_COMMAND
+import com.example.timer.service.MyService
 import java.util.concurrent.TimeUnit
 
 object model {
@@ -24,4 +29,22 @@ object model {
     TimeUnit.MILLISECONDS.toMinutes(this),
     TimeUnit.MILLISECONDS.toSeconds(this) % 60
   )
+
+  fun Context.foregroundStartService(command:String){
+    val intent = Intent(this, MyService::class.java)
+    if (command == "Start"){
+      intent.putExtra(INTENT_COMMAND,command)
+
+      if (Build.VERSION.SDK_INT >=Build.VERSION_CODES.O){
+        this.startForegroundService(intent)
+      }
+      else{
+        this.startService(intent)
+      }
+    }else if (command=="Exit"){
+      intent.putExtra(INTENT_COMMAND,command)
+
+      this.stopService(intent)
+    }
+  }
 }
--- app/src/main/java/com/example/timer/service/myService.kt
+++ app/src/main/java/com/example/timer/service/myService.kt
@@ -1,2 +1,22 @@
 package com.example.timer.service
 
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+const val INTENT_COMMAND = "Command"
+const val INTENT_COMMAND_EXIT = "Exit"
+const val INTENT_COMMAND_REPLY = "Reply"
+const val INTENT_COMMAND_ACHIEVE = "Achieve"
+
+private const val NOTIFICATION_CHANNEL_GENERAL = "Checking"
+private const val CODE_FOREGROUND_SERVICE = 1
+private const val CODE_REPLY_INTENT = 2
+private const val CODE_ACHIEVE_INTENT = 3
+
+class MyService : Service(){
+    override fun onBind(p0: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+
+}
